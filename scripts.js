9985ae68aa048fc650f5ab0ef5ee888f6856dae4
Aaron Williams <polarizedcuriosity@gmail.com>
2023-11-11 01:13:31 -0800

    added some clock formating scripts.

--- scripts.js
+++ scripts.js
@@ -6,6 +6,21 @@ function clock() {
   var seconds = today.getSeconds();
   let period = "AM";
 
+  // Set AM/PM
+  if (hours >= 12) {
+    period = "PM";
+  }
+
+  // Set 12 hour clock format
+
+  hours = hours > 12 ? hours % 12 : hours;
+
+  // Add 0 for values lower then 10
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
   document.querySelector(".hours").innerHTML = hours;
   document.querySelector(".minutes").innerHTML = minutes;
   document.querySelector(".seconds").innerHTML = seconds;
